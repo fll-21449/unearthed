379c5968de810a51c0ef8cd2da209bbcc5ce1ea7
Rocket Fish <hi@rocketfi.sh>
2025-12-27 14:13:37 -0500

    Update copy of SPIKE python programs

--- unearthed robot base.py
+++ unearthed robot base.py
@@ -6,7 +6,7 @@ import motor
 
 SPEED = 70
 
-PROGRAM_NUMBER = 3
+PROGRAM_NUMBER = 5
 
 async def main():
     robot = jones()
@@ -34,23 +34,28 @@ async def main():
 
 
 async def the_flyswatter(robot, back_motor): # yellow thing on the back left wheel on the 2nd thick line
-    await robot.drive_forward(15)
+    await robot.drive_forward(13)
     await robot.turn_left(50)
     await robot.drive_forward(20)
     await robot.turn_right(60)
     await robot.drive_forward(42)
+
     await robot.turn_left(102)
+    # await motor.run_for_degrees(port.B,-360,100)
+
     await robot.drive_forward(4)
     await robot.turn_right(90)
     await robot.drive_backward(15) #drives straight back thing
     await robot.turn_right(22)
     await robot.drive_backward(13)
-    await robot.turn_right(20)
+    await robot.turn_right(19)
     await robot.drive_backward(18.5,speed=100)
     await robot.turn_right(80)
-    await motor.run_for_degrees(back_motor, 360, 1000)
-    await robot.turn_left(60)
-    await robot.turn_right(60)
+    await motor.run_for_degrees(back_motor, 400, 1000)
+    #await robot.turn_left(40)
+    #await robot.turn_right(40)
+    # await motor.run_for_degrees(port.B,-360,100)
+    await motor.run_for_degrees(port.B,200,1000)
     await robot.drive_forward(30)
     await robot.turn_left(45)
     await robot.drive_forward(35)
@@ -156,16 +161,24 @@ async def bulldozer(robot, front_motor):
     await robot.drive_forward(15)
     await robot.turn_left(5)
 
-async def earthmover (robot,front_motor, back_motor): #back right weel on 3rd bold line.
-    await motor.run_for_time(front_motor,1500,-100)
-    await robot.drive_forward(62.4)
+async def earthmover_motor (front_motor, back_motor):
+    await motor.run_for_time(front_motor,1500,-500)
+    await motor.run_for_degrees(back_motor, -70, 300)
+
+
+async def earthmover (robot, front_motor, back_motor): #back right weel on 3rd bold line.
+    a = earthmover_motor (front_motor, back_motor)
+    b = robot.drive_forward(62.4)
+    runloop.run(a,b)
     await robot.turn_left(46)
     await robot.drive_forward(13)
     await motor.run_for_degrees(front_motor, 50, 1100)
-    await motor.run_for_degrees(back_motor, 70, 300)
-    await motor.run_for_degrees(back_motor, -80, 1025)
     await robot.drive_backward(8.5)
     await robot.turn_right(45)
+    await motor.run_for_degrees(back_motor, 140, 300)
+    await motor.run_for_degrees(back_motor, -80, 1025)
+    #await robot.drive_backward(8.5)
+    #await robot.turn_right(45)
     await robot.drive_backward(70)
     # await robot.turn_right(51)
     # await robot.drive_backward(18)
